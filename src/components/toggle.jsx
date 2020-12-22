285f0e98aff059f19628084f00823d44959c931f
Kuba Krzywanski <kszygy@gmail.com>
2020-12-22 19:33:23 +0000

    toggle created and moved to contact section

---
+++ src/components/toggle.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+
+const ToggleLanguage = () => {
+  const [alignment, setAlignment] = useState('ENG');
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+      // style={{ color: 'gray' }}
+    >
+      <ToggleButton value="ENG" aria-label="English">
+        ENG
+      </ToggleButton>
+      <ToggleButton value="PL" aria-label="Polish">
+        PL
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
+
+export default ToggleLanguage;
--- src/containers/Contact/index.jsx
+++ src/containers/Contact/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import labels from '../../assets/labels';
 
+import ToggleLanguage from '../../components/toggle';
+
 const Contact = () => (
-  <div className="background__footer position__footer">
-    <div className="component">
-      <p>{labels.contactNumber}</p>
-      <p>{labels.contactEmail}</p>
-      <p>{labels.contactAdress}</p>
+  <div>
+    <div className="background__footer position__footer">
+      <div className="position__toggle">
+        <ToggleLanguage />
+      </div>
+      <div className="component">
+        <p>{labels.contactNumber}</p>
+        <p>{labels.contactEmail}</p>
+        <p>{labels.contactAdress}</p>
+      </div>
     </div>
   </div>
 );
