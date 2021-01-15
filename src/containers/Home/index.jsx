d6f9b064d8abd724a56a10cb0cc791e14b465236
Kuba Krzywanski <kszygy@gmail.com>
2021-01-15 13:31:04 +0000

    background-image adjusted, ok

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,13 +2,7 @@ import React from 'react';
 import labels from '../../assets/labels';
 
 const AboutMe = () => (
-  <div
-    className="component"
-    style={{
-      background: "url('../../assets/IMG/cooking_background') contain",
-      height: '70vh'
-    }} //W style.scss wywala apke, tutaj nie potrafie wyswietlic tego .jpg jako background-image !!HELP!!
-  >
+  <div className="component background__home">
     <div className="position__bio">
       <div className="position__header">
         <h3>{labels.bioHeader}</h3>
@@ -16,9 +10,6 @@ const AboutMe = () => (
       <div>
         <p>{labels.bioAboutMe} </p>
       </div>
-    </div>
-    <div>
-      <img src="../../assets/IMG/cooking_background" alt="obrazek" />
     </div>
   </div>
 );
