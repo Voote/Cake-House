2ec84d9bbf8e8a2966841be355bfa49c0baa1a14
Kuba Krzywanski <kszygy@gmail.com>
2021-02-03 13:27:32 +0000

    responsive header added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './assets/styles.scss';
 import Header from './components/header';
@@ -7,19 +7,21 @@ import Contact from './containers/Contact';
 import AboutMe from './containers/Home';
 
 const App = () => {
-  const [language, setLanguage] = React.useState('ENG');
-  // React.useEffect(() => {
-  //   languageSelector();
-  // }, []);
-  // const languageSelector = (languageCode) => {
-  //   setLanguage(languageCode);
-  // };
+  const [language, setLanguage] = useState('ENG');
+  // const [load, setLoad] = useState(false);
   const languageSelector = (languageCode) => {
-    language === languageCode
-      ? console.log('Sukces')
-      : setLanguage(languageCode);
-    console.log('language: ', language, 'languageCode: ', languageCode);
+    setLanguage(languageCode);
   };
+  useEffect((languageCode) => {
+    languageSelector(languageCode);
+  }, []);
+
+  // const languageSelector = (languageCode) => {
+  //   language === languageCode
+  //     ? console.log('Sukces')
+  //     : setLanguage(languageCode);
+  //   console.log('language: ', language, 'languageCode: ', languageCode);
+  // };
 
   return (
     <div>
