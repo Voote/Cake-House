ef64879019ea15075648af99866648663bd140aa
Kuba Krzywanski <kszygy@gmail.com>
2021-02-12 10:55:23 +0000

    first instalation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { themeDark, themeLight } from './assets/theme';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
 import App from './App';
 
-ReactDOM.render(
-  <MuiThemeProvider theme={themeLight}>
-    <CssBaseline />
-    <App />
-  </MuiThemeProvider>,
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <MuiThemeProvider theme={themeLight}>
+      <CssBaseline />
+      <App />
+    </MuiThemeProvider>
+  </Provider>,
   document.getElementById('root')
 );
